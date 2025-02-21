ab83c47a236be6144231e5d523b26a82d4266de3
Riddh Modi <riddhmodi2003@gmail.com>
2025-02-21 21:27:03 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,16 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0", // Ensure it binds to the correct IP
-    port: process.env.PORT || 5173, // Use Render’s port
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    allowedHosts: ["lesson-planner-p2te.onrender.com"],
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
   },
 })
